d8ec21644445c86edafc0b209dfecbaf106acac8
Bhumi Patel <bhumip214@gmail.com>
2019-12-30 00:38:14 -0800

    Update api.ts

--- src/components/api.ts
+++ src/components/api.ts
@@ -64,6 +64,74 @@ export enum ReleaseDatePrecision {
   Day = "day"
 }
 
+// album
+export interface IAlbum {
+  album_type: string;
+  artists: Artist[];
+  available_markets: string[];
+  copyrights: Copyright[];
+  external_ids: ExternalIDS;
+  external_urls: ExternalUrls;
+  genres: any[];
+  href: string;
+  id: string;
+  images: Image[];
+  label: string;
+  name: string;
+  popularity: number;
+  release_date: Date;
+  release_date_precision: string;
+  total_tracks: number;
+  tracks: Tracks;
+  type: string;
+  uri: string;
+}
+
+export interface Artist {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: ArtistType;
+  uri: string;
+}
+
+export interface Copyright {
+  text: string;
+  type: string;
+}
+
+export interface ExternalIDS {
+  upc: string;
+}
+
+export interface ITracks {
+  href: string;
+  items: Item[];
+  limit: number;
+  next: null;
+  offset: number;
+  previous: null;
+  total: number;
+}
+
+export interface Item {
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_local: boolean;
+  name: string;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+}
+
 //FeaturedList
 export interface FeaturedList {
   href: string;
