80b23a801fc8852a655be879d201a6c60af120b9
Bhumi Patel <bhumip214@gmail.com>
2020-01-02 15:19:30 -0800

    Update SideNavBar.tsx

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Navigation } from "baseui/side-navigation";
 import { styled } from "baseui";
 import { H4 } from "baseui/typography";
-import { NavLink } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 const LeftAligned = styled("div", {
   textAlign: "left",
@@ -20,7 +20,9 @@ const ItemTitle = styled("span", {
 });
 
 const SideNavBar = () => {
-  const [activeItemId, setActiveItemId] = React.useState("#newReleases");
+  const history = useHistory();
+  const location = useLocation();
+
   return (
     <LeftAligned>
       <H4 padding={"20px 40px"} margin={0}>
@@ -30,98 +32,85 @@ const SideNavBar = () => {
         items={[
           {
             title: (
-              <NavLink
-                style={{ textDecoration: "none", color: "white" }}
-                to="/new-releases"
-              >
-                <NavItem>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-disc"
-                  >
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <circle cx="12" cy="12" r="3"></circle>
-                  </svg>
-                  <ItemTitle> New Releases</ItemTitle>
-                </NavItem>
-              </NavLink>
+              <NavItem>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-disc"
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <circle cx="12" cy="12" r="3"></circle>
+                </svg>
+                <ItemTitle> New Releases</ItemTitle>
+              </NavItem>
             ),
-            itemId: "#newReleases"
+            itemId: "/new-releases"
           },
           {
             title: (
-              <NavLink
-                style={{ textDecoration: "none", color: "white" }}
-                to="/featured-list"
-              >
-                <NavItem>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-list"
-                  >
-                    <line x1="8" y1="6" x2="21" y2="6"></line>
-                    <line x1="8" y1="12" x2="21" y2="12"></line>
-                    <line x1="8" y1="18" x2="21" y2="18"></line>
-                    <line x1="3" y1="6" x2="3.01" y2="6"></line>
-                    <line x1="3" y1="12" x2="3.01" y2="12"></line>
-                    <line x1="3" y1="18" x2="3.01" y2="18"></line>
-                  </svg>
+              <NavItem>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-list"
+                >
+                  <line x1="8" y1="6" x2="21" y2="6"></line>
+                  <line x1="8" y1="12" x2="21" y2="12"></line>
+                  <line x1="8" y1="18" x2="21" y2="18"></line>
+                  <line x1="3" y1="6" x2="3.01" y2="6"></line>
+                  <line x1="3" y1="12" x2="3.01" y2="12"></line>
+                  <line x1="3" y1="18" x2="3.01" y2="18"></line>
+                </svg>
 
-                  <ItemTitle> Featured List</ItemTitle>
-                </NavItem>
-              </NavLink>
+                <ItemTitle> Featured List</ItemTitle>
+              </NavItem>
             ),
-            itemId: "#featuredList"
+            itemId: "/featured-list"
           },
           {
             title: (
-              <NavLink
-                style={{ textDecoration: "none", color: "white" }}
-                to="/categories"
-              >
-                <NavItem>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-music"
-                  >
-                    <path d="M9 18V5l12-2v13"></path>
-                    <circle cx="6" cy="18" r="3"></circle>
-                    <circle cx="18" cy="16" r="3"></circle>
-                  </svg>
-                  <ItemTitle> Categories</ItemTitle>
-                </NavItem>
-              </NavLink>
+              <NavItem>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-music"
+                >
+                  <path d="M9 18V5l12-2v13"></path>
+                  <circle cx="6" cy="18" r="3"></circle>
+                  <circle cx="18" cy="16" r="3"></circle>
+                </svg>
+                <ItemTitle> Categories</ItemTitle>
+              </NavItem>
             ),
-
-            itemId: "#categories"
+            itemId: "/categories"
           }
         ]}
-        activeItemId={activeItemId}
-        onChange={({ item }) => setActiveItemId(item.itemId)}
+        activeItemId={location.pathname || "/new-releases"}
+        onChange={({ event, item }) => {
+          event.preventDefault();
+          history.push(item.itemId);
+        }}
       />
     </LeftAligned>
   );
